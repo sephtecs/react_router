282fb930e2fbcd00da17757843001594ebab67e2
Joseph <josephtecson18@gmail.com>
2021-09-08 14:00:16 -0700

    add changes

--- src/App.js
+++ src/App.js
@@ -3,41 +3,34 @@ import {
   BrowserRouter,
   Switch,
   Route,
-  Link 
+  Link,
+  useParams 
 } from "react-router-dom";
 
 const Home = (props) => {
-  return (<h1 style = {{color: "red"}}>We are in our Home Component</h1>);
+  return (<h1>Welcome</h1>);
 }
 
-const About = (props) => {
-  return (<h1 style = {{color: "blue"}}>We are in our About Component</h1>);
+const String = (props) => {
+  const { id } = useParams();
+  if (!isNaN(id)) {
+    return <h2> Your number is: { id }</h2>
+  }
+  else {
+    return (<h2>Your word is: { id }  </h2>);
+  }
 }
 
 function App() {
+
   return (
     <BrowserRouter>
-      <h1>Routing Example</h1>
-      <p>
-        <Link to="/home">Home</Link>
-        &nbsp;|&nbsp;
-        <Link to="/about">About</Link>
-        &nbsp;|&nbsp;
-        <Link to="/number">Number</Link>
-        &nbsp;|&nbsp;
-        <Link to="/others">Other</Link>   
-      </p>
+
       <Switch>
-        <Route path = "/about">
-          <About />
+        <Route path = "/:id">
+          <String />
         </Route>
-        <Route exact path = "/home">
-          <Home />
-        </Route>
-        <Route path = "/others">
-          <About />
-        </Route>
-        <Route exact path = "/thistoo">
+        <Route exact path = "/">
           <Home />
         </Route>
       </Switch>
