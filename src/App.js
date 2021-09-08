476b5b5cb4ee1f801d854c3ef24b346574c45c8a
Joseph <josephtecson18@gmail.com>
2021-09-07 22:48:50 -0700

    add files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link 
+} from "react-router-dom";
+
+const Home = (props) => {
+  return (<h1 style = {{color: "red"}}>We are in our Home Component</h1>);
+}
+
+const About = (props) => {
+  return (<h1 style = {{color: "blue"}}>We are in our About Component</h1>);
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <h1>Routing Example</h1>
+      <p>
+        <Link to="/home">Home</Link>
+        &nbsp;|&nbsp;
+        <Link to="/about">About</Link>
+        &nbsp;|&nbsp;
+        <Link to="/number">Number</Link>
+        &nbsp;|&nbsp;
+        <Link to="/others">Other</Link>   
+      </p>
+      <Switch>
+        <Route path = "/about">
+          <About />
+        </Route>
+        <Route exact path = "/home">
+          <Home />
+        </Route>
+        <Route path = "/others">
+          <About />
+        </Route>
+        <Route exact path = "/thistoo">
+          <Home />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
